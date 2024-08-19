46a0d76ae897d2c1d488582d5bc26be76d70896d
TauraiBwe <47793352+TauraiBwe@users.noreply.github.com>
2024-08-19 15:16:24 +0300

    Add files via upload

---
+++ Bwerinofa Jupyter.py
@@ -0,0 +1,163 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+# 
+# Objectives:
+# 
+# List popular languages that Data Scientists use.
+# List commonly used libraries used by Data Scientists.
+# Comment on Data Science tools.
+# 
+
+# In[1]:
+
+
+Some of the popular languages that Data Scientists use are:
+
+Python.
+R.
+SQL.
+Java.
+Julia.
+Scala.
+C/C++.
+JavaScript.
+
+
+# Some of the popular languages that Data Scientists use are:
+# 
+# Python.
+# R.
+# SQL.
+# Java.
+# Julia.
+# Scala.
+# C/C++.
+# JavaScript.
+
+# commonly used libraries used by Data Scientists include:
+# 
+# TensorFlow.
+# NumPy.
+# SciPy.
+# Pandas.
+# Matplotlib.
+# Keras.
+# SciKit-Learn.
+# PyTorch.
+# Scrapy.
+# BeautifulSoup.
+# LightGBM.
+# ELI5.
+# Theano.
+# NuPIC.
+
+# Data Science Tools:
+# 
+# Data Science Tools		
+# SAS. It is one of those data science tools which are specifically designed for statistical operation		
+# Apache Spark		
+# BigML
+
+# a few examples of evaluating arithmetic expressions in Python
+# # Arithmetic operations
+# code = compile("5 + 4", "<string>", "eval")
+# eval(code)
+# # Result: 9
+# 9
+# code1 = compile("(5 + 7) * 2", "<string>", "eval")
+# eval(code1)
+# # Result: 24
+# 24
+# import math
+# # Volume of a sphere
+# code2 = compile("4 / 3 * math.pi * math.pow(25, 3)", "<string>", "eval")
+# eval(code2)
+# # Result: 65449.84694978735
+# 65449.84694978735
+
+# This a simple arithmetic expression to mutiply then add integers
+# 
+# (5*4)+5
+# # Result: 17
+# 
+
+# This a simple arithmetic expression to mutiply then add integers
+# 
+# (3*4)+5
+# 
+# 
+
+# In[2]:
+
+
+days = 0
+hours = 0
+mins = 0
+
+time = 200
+#days = time / 1440
+leftover_minutes = time % 1440
+hours = leftover_minutes / 60
+#mins = time - (days*1440) - (hours*60)
+print(str(days) + " days, " + str(hours) + " hours, " + str(mins) +  " mins. ")
+
+# Result: 3.3333333333333335 hours
+
+
+# In[3]:
+
+
+(3*4)+5
+
+
+# In[4]:
+
+
+# Arithmetic operations
+code = compile("5 + 4", "<string>", "eval")
+eval(code)
+
+code1 = compile("(5 + 7) * 2", "<string>", "eval")
+eval(code1)
+
+import math
+# Volume of a sphere
+code2 = compile("4 / 3 * math.pi * math.pow(25, 3)", "<string>", "eval")
+eval(code2)
+
+
+# In[5]:
+
+
+# Arithmetic operations
+code = compile("5 + 4", "<string>", "eval")
+eval(code)
+
+
+
+# In[6]:
+
+
+(3*4)+5
+# Result: 17
+
+
+# Below are the objectives of the course
+# #learn data science
+# #learn R
+# #learn Python
+
+# The obcetives of the course are
+# learn python
+# learn data science
+# learn R
+
+# The author of this is Taurai Bwerinofa
+
+# In[ ]:
+
+
+
+
